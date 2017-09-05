182388e02910c5d7b4f605021d99c3664972387a
ptxd <ptxdas@gmail.com>
2017-09-05 14:50:23 -0700

    btn

--- src/HOCs/Support.js
+++ src/HOCs/Support.js
@@ -4,15 +4,23 @@ import PaperWrapper from '../components/paper/paper';
 import FlatBtn from '../components/buttons/flatbutton';
 
 class Support extends Component {
+    
     constructor(props){
         super(props);
-        this.state={value:null};
-        this.handleClick = this.handleClick.bind(this);
+        this.state={value:0};
+        this.clickValue = this.clickValue.bind(this);
+        this.customValue = this.customValue.bind(this);
     }
     
-    handleClick(e){
-        console.log(e.target.value);
+    clickValue(e){
+        this.setState({value:e});
     }
+
+    customValue(e){
+        e.preventDefault();
+        this.setState({value:e.target.value});
+    }
+
     render() {
         return (
             <div className="support">
@@ -21,7 +29,13 @@ class Support extends Component {
                     <p>Thank You For The Support!</p>
                 </div>
                 <div>
-                    <FlatBtn label="$100" type="submit" value="100" onClick={this.handleClick}/>
+                    <FlatBtn label={100} handleClick={()=>{this.clickValue('100')}}/>
+                    <FlatBtn label={50} handleClick={()=>{this.clickValue('50')}}/>
+                    <FlatBtn label={20} handleClick={()=>{this.clickValue('20')}}/>
+                    <FlatBtn label={10} handleClick={()=>{this.clickValue('10')}}/>
+                    <FlatBtn label={5} handleClick={()=>{this.clickValue('5')}}/>
+                    <form><input type="text" onChange={this.customValue}/></form>
+                    <p>Contribution: ${this.state.value}</p>
                     <PaperWrapper/>
                 </div>
             </div>
@@ -30,3 +44,8 @@ class Support extends Component {
 }
 
 export default Support;
+
+// <FlatBtn label="$50"  onClick={clickValue('50')}/>
+// <FlatBtn label="$20"  onClick={clickValue('20')}/>
+// <FlatBtn label="$10"  onClick={clickValue('10')}/>
+// <FlatBtn label="$5"  onClick={clickValue('10')}/>
--- src/components/buttons/flatbutton.js
+++ src/components/buttons/flatbutton.js
@@ -3,14 +3,16 @@ import FlatButton from 'material-ui/FlatButton';
 
 const FlatBtn = (props) =>{
         return (
-            <div>
+
                 <FlatButton
                     backgroundColor="#4FC3F7"
                     hoverColor="#03A9F4"
                     label={props.label}
                     type="submit"
-                />
-            </div>
+                    onClick={props.handleClick}
+                >
+                </FlatButton>
+
         );
 }
 
